5e5f65f9a65023f159ba8472aabf1d1cf6e813f7
Shir Lupo <shir.lupo@snyk.io>
2026-04-09 14:10:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ var cons = require('consolidate');
 var app = express();
 var routes = require('./routes');
 
+var secret = "1234424j_x";
 // all environments
 app.set('port', process.env.PORT || 3001);
 app.engine('ejs', ejsEngine);
